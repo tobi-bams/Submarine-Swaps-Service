67aa8986b677151024a6a9c06516ad9f6eb6303e
Bamidele Oluwatobi <teebams49@gmail.com>
2022-06-06 15:25:34 +0100

    init: frontend initialized with typescript and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl bg-red-200">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
