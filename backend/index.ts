2884d10f0a4d59f1d8f9729e18d5e7330bb3cc85
Bamidele Oluwatobi <teebams49@gmail.com>
2022-06-08 12:51:28 +0100

    update: Express server up and running

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,16 @@
-console.log("Happy to be here");
+import express, { Application } from "express";
+import cors from "cors";
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Express server is running" });
+});
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server up and running");
+});
