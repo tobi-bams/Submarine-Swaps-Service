1195b00e92bea20714f12ddb7e40e8c7bfdd2e7f
Bamidele Oluwatobi <teebams49@gmail.com>
2022-08-06 14:14:23 +0100

    changed port number of bitcoin node to that of test, added network check for regtest

--- backend/index.ts
+++ backend/index.ts
@@ -22,8 +22,8 @@ app.get("/", (req, res) => {
 // testRPC();
 const invoice =
   "lnbcrt1u1p3vs2t3pp5xfrgypr7hyxammgyv30xgg04zqcmkt32xjts44wjnmfm3hf8v0fqdqqcqzpgxqyz5vqsp5ul965n67wca0ree6agdevh77y8k4lf6vy64w8aer92suvm3fvfvq9qyyssq43saa5gf7w68d2l66kclgaxjjjhjyleq0yxnmzh4e3ucwegjqtx9myfh7trynr624yfkttkwy7nrtgydrwjyyvgz07n3ve0glmfu0fqqzzzq7q";
-ClaimFundsController(invoice);
-RefundFundsController(invoice);
+// ClaimFundsController(invoice);
+// RefundFundsController(invoice);
 
 const PORT = process.env.port || 5000;
 
