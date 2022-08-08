86259e6327435bddf874953c6109ba0fe0f18884
Bamidele Oluwatobi <teebams49@gmail.com>
2022-08-08 22:34:46 +0100

    update: verify lightning address updateed

---
+++ backend/routes/address.ts
@@ -0,0 +1,7 @@
+import express, { Router, Request, Response } from "express";
+
+const route: Router = express.Router();
+
+route.post("/generate", (req: Request, res: Response) => {});
+
+export default route;
